f92755a51aa8d10784cd08f358f434413d2b8ce7
Jaimewanna <mrjaimewanna@gmail.com>
2021-11-13 18:26:59 -0500

    Prueba commit

--- src/main/java/com/nttdata/App.java
+++ src/main/java/com/nttdata/App.java
@@ -1,4 +1,4 @@
-package org.example;
+package com.nttdata;
 
 /**
  * Hello world!
---
+++ src/test/java/com/nttdata/AppTest.java
@@ -0,0 +1,15 @@
+package com.nttdata;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+
+}
