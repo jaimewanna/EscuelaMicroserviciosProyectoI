25910019d92974f2c3263d62423fc89f0d771591
Jaimewanna <mrjaimewanna@gmail.com>
2021-11-14 16:00:23 -0500

    creacion constructor operationservice

--- src/main/java/com/nttdata/service/OperationService.java
+++ src/main/java/com/nttdata/service/OperationService.java
@@ -1,13 +1,25 @@
 package com.nttdata.service;
 
 import com.nttdata.proxy.AccountProxy;
+import com.nttdata.proxy.MovementsProxy;
+import com.nttdata.proxy.ClientProxy;
+
+import java.util.stream.Collectors;
 
 public class OperationService {
 
     private AccountProxy accountProxy;
+    private MovementsProxy movementsProxy;
+    private ClientProxy clientProxy;
 
     public  OperationService(){
         accountProxy = new AccountProxy();
+        movementsProxy = new MovementsProxy();
+        clientProxy = new ClientProxy();
+    }
+
+    public void getAllBalanceClient(int clientId){
+
     }
 
 
