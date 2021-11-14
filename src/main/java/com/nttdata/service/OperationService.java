e257a0348b76d01314df54f59a4bf1ab74de7bf9
Jaimewanna <mrjaimewanna@gmail.com>
2021-11-14 17:54:34 -0500

    comentarios metodo

--- src/main/java/com/nttdata/service/OperationService.java
+++ src/main/java/com/nttdata/service/OperationService.java
@@ -19,6 +19,7 @@ public class OperationService {
         clientProxy = new ClientProxy();
     }
 
+    //Se traen todas las cuentas bancarias, se filtran por el id del cliente, se convierten en lista y se muestran
     public void getAllProductBalanceClient(int clientId){
         accountProxy.accountsGet()
                 .filter(account -> account.getClientId().equals(clientId))
@@ -26,6 +27,7 @@ public class OperationService {
                 .forEach(System.out::println);
     }
 
+    //Se trae la relación de todos los movimientos, luego se fitlran por id de la cuenta y se muestran
     public void getAllMovementsFromAccount(int accountId){
         movementsProxy.getAllMovements()
                 .filter(movements -> movements.getAccountId().equals(accountId))
@@ -33,6 +35,8 @@ public class OperationService {
                 .forEach(System.out::println);
     }
 
+    //Se traen todos los creditos, se obtiene la tarjeta por su id, se revisa si el consumo mas el monto actual
+    //son menores que el limite y en ese caso se imprime el nuevo monto
     public void addConsumetoCard(int cardId, Double consume){
         accountProxy.creditsGetAll()
                 .filter(credit -> credit.getId().equals(cardId))
